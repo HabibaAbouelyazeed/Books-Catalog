9837861e35d19773543806238c7785e1affc7fd3
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-09-06 19:52:19 +0300

    fet/ Book Details Page

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Software Books Catalog
+# Books Catalog
  Web-based application that utilizes the Google Books API to showcase a catalog of software development books.
+
+
+## Tools & Tech
+
+.HTML
+.CSS
+.Vanilla JS
+.Google Books API
+.AJAX
--- js/script.js
+++ js/script.js
@@ -3,11 +3,7 @@ const promotionBannerImg = document.querySelector("#promotion-banner");
 const searchInpt = document.querySelector("#searchInpt");
 const searchBtn = document.querySelector(".search-bar .search-btn");
 const booksCardsHolderDiv = document.querySelector("#booksCardsHolder");
-const seeMoreBtn = document.querySelector('.seeMore-btn');
 
-
-// const pageLimit = 20;
-// let currentPageItems = pageLimit;
 const bannerImagesSRC = [
   "../assets/books-banner1.png",
   "../assets/books-banner2.png",
@@ -15,94 +11,80 @@ const bannerImagesSRC = [
 const ApiBasicPart = "https://www.googleapis.com/books/v1/volumes?q=";
 let searchQuery = "software+development";
 
-// 1. Create a new XMLHttpRequest object
 let xhr = new XMLHttpRequest();
 
 let slideIndex = 0;
 let data = [];
+let currentbookID = "";
 
-if(document.body.id === "indexPage"){
-  onPageLoad();
-}
+if (document.body.id === "indexPage") {
+  // onPageLoad();
 
-searchBtn.addEventListener("click", function () {
-  if (searchInpt.value.trim().length > 0) {
-    searchInpt.value = searchInpt.value.trim();
-    searchQuery = formatSearchQuery(searchInpt.value);
+  searchBtn.addEventListener("click", function () {
+    if (searchInpt.value.trim().length > 0) {
+      searchInpt.value = searchInpt.value.trim();
+      searchQuery = formatSearchQuery(searchInpt.value);
 
-    // 2. Configure it: GET-request for the URL /article/.../load
-    xhr.open("GET", `${ApiBasicPart}${searchQuery}`);
-
-    xhr.responseType = "json";
-    // 3. Send the request over the network
-    xhr.send();
-
-    // 4. This will be called after the response is received
-    xhr.onload = function () {
-      if (xhr.status != 200) {
-        // analyze HTTP status of the response
-        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-      } else {
-        // show the result
-        data = xhr.response;
-        console.log(data.items.length);
-        booksCardsHolderDiv.innerHTML = '';
-        for(let i = 0; i < data.items.length; i++){
-          booksCardsHolderDiv.appendChild(createBookCard(data.items[i]));
-        } 
-      }
-    };
-
-    xhr.onprogress = function (event) {
-      booksCardsHolderDiv.innerHTML = "<p>Loading ... </p>";
-    };
-
-    xhr.onerror = function () {
-      alert("Request failed");
-    };
-  }
-});
-
-// seeMoreBtn.addEventListener('click',function(){
-//   displayNextLimit();
-// });
-
-
-/*********************************************************************************************************************************/
-function onPageLoad(){
-  createBannerDots();
-  dotHolderDiv.childNodes[slideIndex].classList.add("active");
-
-  setInterval(function(){
-      slideShow();
-  }, 3500);
       xhr.open("GET", `${ApiBasicPart}${searchQuery}`);
-
       xhr.responseType = "json";
       xhr.send();
-  
+
       xhr.onload = function () {
         if (xhr.status != 200) {
-          alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+          alert(`Error ${xhr.status}: ${xhr.statusText}`);
         } else {
           data = xhr.response;
-          // console.log(data.items.length);
-          booksCardsHolderDiv.innerHTML = '';
-          for(let i = 0; i < data.items.length; i++){
-            if(data.items[i] != null){
-              booksCardsHolderDiv.appendChild(createBookCard(data.items[i]));
-            }
-          } 
+          booksCardsHolderDiv.innerHTML = "";
+          for (let i = 0; i < data.items.length; i++) {
+            booksCardsHolderDiv.appendChild(createBookCard(data.items[i]));
+          }
         }
       };
+
       xhr.onprogress = function (event) {
         booksCardsHolderDiv.innerHTML = "<p>Loading ... </p>";
       };
+
       xhr.onerror = function () {
         alert("Request failed");
       };
+    }
+  });
 }
 
+/*********************************************************************************************************************************/
+function onPageLoad() {
+  createBannerDots();
+  dotHolderDiv.childNodes[slideIndex].classList.add("active");
+
+  setInterval(function () {
+    slideShow();
+  }, 3500);
+  xhr.open("GET", `${ApiBasicPart}${searchQuery}`);
+
+  xhr.responseType = "json";
+  xhr.send();
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      data = xhr.response;
+      booksCardsHolderDiv.innerHTML = "";
+      for (let i = 0; i < data.items.length; i++) {
+        if (data.items[i] != null) {
+          booksCardsHolderDiv.appendChild(createBookCard(data.items[i]));
+        }
+      }
+    }
+  };
+  xhr.onprogress = function (event) {
+    booksCardsHolderDiv.innerHTML = "<p>Loading ... </p>";
+  };
+  xhr.onerror = function () {
+    alert("Request failed");
+  };
+}
 
 function createBookCard(bookItem) {
   const bookItemDiv = document.createElement("div");
@@ -110,18 +92,18 @@ function createBookCard(bookItem) {
   bookTemp += `
       <div class="book-Item-Card">
       `;
-  if(bookItem.volumeInfo.hasOwnProperty('imageLinks')){
+  if (bookItem.volumeInfo.hasOwnProperty("imageLinks")) {
     bookTemp += `
       <div class="book-image-holder">
           <img src="${bookItem.volumeInfo.imageLinks.thumbnail}" alt="bookThumbnail" class="image-fluid">
       </div>
       `;
-  } 
+  }
   bookTemp += `
     <div class="book-info">
             <h2 class="book-name">${bookItem.volumeInfo.title}</h2>
   `;
-  if(bookItem.volumeInfo.hasOwnProperty('description')){
+  if (bookItem.volumeInfo.hasOwnProperty("description")) {
     bookTemp += `
 
       <p class="book-details">
@@ -133,19 +115,28 @@ function createBookCard(bookItem) {
   bookTemp += `
             <h4 class="book-author"><span><i>Author: </i></span>${bookItem.volumeInfo.authors[0]}</h4>
         </div>
-        <div  onclick="getBookID(this)" data-info="${bookItem.id}" class="moreDetails-btn btn rounded-pill">Book Details</div>
+        <div onclick="goToDetailsPage(this)" data-info="${bookItem.id}" class="moreDetails-btn btn rounded-pill">Book Details</div>
     </div>
   `;
   bookItemDiv.innerHTML = bookTemp;
   return bookItemDiv;
 }
-//href="bookDetails.html"
 
-function getBookID(book){
-  let bookID = book.getAttribute('data-info');
-  console.log(bookID);
-  return bookID;
+function goToDetailsPage(book) {
+  let bookID = book.getAttribute("data-info");
+  let redirectUrl = "./bookDetails.html?id=" + bookID;
+  document.location.href = redirectUrl;
 }
+
+// function getBookID(book){
+//   let bookID = book.getAttribute('data-info');
+//   currentbookID = bookID
+//   // return bookID;
+// }
+
+// function retriveBookData(){
+//   console.log(currentbookID);
+// }
 
 function formatSearchQuery(queryString) {
   let formatedString = "";
@@ -180,4 +171,97 @@ function createBannerDots() {
   }
 
   return dots;
+}
+
+// Book details functions
+function onDetailsPageLoad() {
+  const bookDetailsSecDiv = document.querySelector("#bookDetailsSec");
+  let extractedId = extractUrlId();
+  let bookUrl = "https://www.googleapis.com/books/v1/volumes/" + extractedId;
+
+  
+  xhr.open("GET", bookUrl);
+  xhr.responseType = "json";
+  xhr.send();
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      bookDetailsSecDiv.innerHTML = "";
+      data = xhr.response;
+      console.log(data);
+      bookDetailsSecDiv.innerHTML = renderBookDetails(data);
+    }
+  };
+
+  xhr.onprogress = function (event) {
+    bookDetailsSecDiv.innerHTML = "<p>Loading ... </p>";
+  };
+
+  xhr.onerror = function () {
+    alert("Request failed");
+  };
+}
+
+function renderBookDetails(book) {
+  let bookTemp = `
+    <h1 class="txt-center">Book details</h1>
+    <div class="main-info">
+    `;
+  if (book.volumeInfo.hasOwnProperty("imageLinks")) {
+    bookTemp += `
+    <div class="bookImage">
+      <img src="${book.volumeInfo.imageLinks.thumbnail}" alt="book Thumbnail" class="image-fluid">
+    </div>
+    `;
+  }
+  bookTemp += `
+    <div class="info">
+      <h2 class="book-title">${book.volumeInfo.title}</h2>
+  `;
+  if (book.volumeInfo.hasOwnProperty("description")) {
+    bookTemp += `
+    <div class="book-description">${book.volumeInfo.description}</div>
+    `;
+  }
+  bookTemp += `
+      </div>
+    </div>`;
+  bookTemp += `
+  <div class="book-authors">
+      <h3>Authors</h3>
+      <ul class="authors-list">
+  `;
+  for (let i = 0; i < book.volumeInfo.authors.length; i++) {
+    bookTemp += `<li>${i+1}.${book.volumeInfo.authors[i]}</li> `;
+  }
+
+  bookTemp += `
+      </ul>
+    </div>
+  `;
+
+  bookTemp += `
+    <div class="extra-info">
+      <div class="publisher">
+        <h3>Publisher:</h3>
+        <p>${book.volumeInfo.publisher}</p>
+      </div>
+
+      <div class="published-date">
+        <h3>Published Date:</h3>
+        <p>${book.volumeInfo.publishedDate}</p>
+      </div>
+    </div>
+  
+  `;
+
+  return bookTemp;
+}
+
+function extractUrlId() {
+  let url = document.location.href;
+  let extractedId = url.split("?")[1].split("=")[1];
+  return extractedId;
 }
